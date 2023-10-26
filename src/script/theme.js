3cc6e98df4fbeabd36fb49c1d36b67ad7191a676
frankuxui <fisdray@gmail.com>
2023-10-26 11:02:10 +0200

    fixed changes

---
+++ src/script/theme.js
@@ -0,0 +1,44 @@
+const HTML = document?.documentElement;
+  const darkIcon = document.getElementById("icon-dark");
+  const lightIcon = document.getElementById("icon-light");
+
+  if ( localStorage.getItem("color-theme") === "dark" || (!("color-theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches) ) {
+    lightIcon?.classList.remove("hidden");
+  } else {
+    darkIcon?.classList.remove("hidden");
+  }
+
+  const trigger = document.getElementById("theme-toggle");
+
+  trigger?.addEventListener("click", function () {
+    
+    //
+    // toggle icons inside button
+    darkIcon?.classList.toggle("hidden");
+    lightIcon?.classList.toggle("hidden");
+    
+    //
+    // if set via local storage previously
+
+    if (localStorage.getItem("color-theme")) {
+      if (localStorage.getItem("color-theme") === "light") {
+        HTML.classList.add("dark");
+        localStorage.setItem("color-theme", "dark");
+      } else {
+        HTML.classList.remove("dark");
+        localStorage.setItem("color-theme", "light");
+      }
+      
+      //
+      // if NOT set via local storage previously
+    } else {
+      if (HTML.classList.contains("dark")) {
+        HTML.classList.remove("dark");
+        localStorage.setItem("color-theme", "light");
+      } else {
+        HTML.classList.add("dark");
+        localStorage.setItem("color-theme", "dark");
+      }
+    }
+  });
+  
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,64 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors'
+
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: "class",
   theme: {
     extend: {
 
-      //
+      // Animation
+
+      animation: {
+        popover: 'popover 0.2s linear',
+        growing: 'growing .75s linear infinite'
+      },
+      
+
       // Colors
 
       colors: {
-        primary: '#6d28d9',
-        error: '#d30b5f',
+        primary: colors.violet[700],
+        'primary-dark': colors.violet[800],
+
+        secondary: colors.gray[200],
+        'secondary-dark': colors.gray[300],
+
+        error: colors.rose[600],
+        'error-dark': colors.rose[700],
+        'error-light': colors.rose[100],
+
+        success: colors.emerald[500],
+        'success-dark': colors.emerald[600],
+        'success-light': colors.emerald[100],
+
         dark: '#101729'
-      }
+      },
+
+      
+      // Keyframes
+
+      keyframes: {
+        popover: {
+          '0%': {
+            transform: 'scale(0.95)'
+          },
+          '100%': {
+            opacity: 1,
+          }
+        },
+        growing: {
+          '0%': {
+            transform: 'scale(0)',
+          },
+          '100%': {
+            transform: 'none',
+            opacity: 1
+          }
+        }
+      },
     }
+    
   },
   plugins: []
 }
