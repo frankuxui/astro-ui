6c077966d651d791979b283da79d8f3eb3f0764f
frankuxui <fisdray@gmail.com>
2023-12-16 12:20:18 +0100

    sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import mdx from '@astrojs/mdx'
-import vercel from '@astrojs/vercel/serverless'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel/serverless';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  site: 'http://astro-ui-three.vercel.app',
+  integrations: [tailwind(), mdx(), sitemap()],
   output: 'server',
   adapter: vercel({
     webAnalytics: {
       enabled: true
     }
   })
-})
+});
